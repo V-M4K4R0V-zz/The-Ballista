94ec4eeecd9528a737a67f973f7f42f8291fd21c
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-02-02 17:20:13 +0100

    ta7ya_ltoilet

--- catapulta.py
+++ catapulta.py
@@ -2,15 +2,34 @@ import socket
 import threading
 import time
 
-#============target info================#
-zombie_IP = input("Enter your target IP : ")
-zombie_PORT = input("Enter the port : ")
+
+#ta7ya l toilet
+print('''
+ ____  ____   ___  ____                      _       _     _           
+|  _ \|  _ \ / _ \/ ___|       ___  ___ _ __(_)_ __ | |_  | |__  _   _ 
+| | | | | | | | | \___ \ _____/ __|/ __| '__| | '_ \| __| | '_ \| | | |
+| |_| | |_| | |_| |___) |_____\__ \ (__| |  | | |_) | |_  | |_) | |_| |
+|____/|____/ \___/|____/      |___/\___|_|  |_| .__/ \__| |_.__/ \__, |
+                                              |_|                |___/ 
+__     __      __  __ _  _   _  ___  _   ____   _____     __
+\ \   / /     |  \/  | || | | |/ / || | |  _ \ / _ \ \   / /
+ \ \ / / ____ | |\/| | || |_| ' /| || |_| |_) | | | \ \ / / 
+  \ V / |____|| |  | |__   _| . \|__   _|  _ <| |_| |\ V /  
+   \_/        |_|  |_|  |_| |_|\_\  |_| |_| \_\\___/  \_/                                                    
+''')
+
+
 print('''
 |-------------------------ATTENTION------------------------------|
-|   1!= you'd better run this script on VPS or RDP               |
+|    1!= you'd better run this script on VPS or RDP              |
 |    2!= if u dont have one of them u should change your IP-add  |
 |----------------------------------------------------------------|
 ''')
+
+#============target info================#
+zombie_IP = input("Enter your target IP : ")
+zombie_PORT = input("Enter the port : ")
+
 
 #============Attack func================#
 def attack():
